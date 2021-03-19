237f4d0ab99cbffea845ff05ff51881ed6b3d55c
Saia Fonua <saia.fonua@C02F20S7MD6M.local>
2021-03-19 10:36:25 -0600

    initial state

--- pages/index.js
+++ pages/index.js
@@ -3,58 +3,53 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const [ isPlaying, setIsPlaying ] = useState(true);
+  const toDOItems = [
+    "15 minute Book of Mormon Reference",
+    "15 minute Prayer to God",
+    "15 Minute Write in Journal",
+    "1 Hour Exericse",
+    "15 Minute Video Games (Nintendo Switch)"
+  ];
+  const [isPlaying, setIsPlaying] = useState(true);
+  const [counter, setCounter] = useState(15);
   useEffect(() => {
     const audio = new Audio('/alarm_tone.mp3');
-    if(isPlaying) {
-      // audio.play();
-
+    if (isPlaying) {
+    audio.play();
     }
   }, [])
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCounter(counter - 1)
+    }, 1000)
+  }, [counter])
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Alarm</title>
         <link rel="icon" href="/spider_senses.png" />
       </Head>
 
       <main className={styles.main}>
+        <p><span>Immediate</span> Task To Do within:</p>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a href="https://nextjs.org">{counter}</a>
+          <h5 className={styles.textSm} href="https://nextjs.org">Seconds</h5>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          {toDOItems[0]}
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Started &rarr;</h3>
           </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>State &rarr;</h3>
+            <p>Has Changed?</p>
           </a>
         </div>
       </main>
@@ -65,8 +60,8 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          You Got This
+          <img src="/action_saia.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
